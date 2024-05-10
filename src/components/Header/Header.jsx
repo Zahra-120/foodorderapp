6c29f5bb7073383a767143f4e5046e9776f1b49f
Zahra Vora <136057143+Zahra-120@users.noreply.github.com>
2024-05-10 16:46:15 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import image from 'src/assets/food1.jpg'
+import image from '../../assets/food1.jpg'
 import Content from '../Content/Content'
 import CartButton from './CartButton'
 
